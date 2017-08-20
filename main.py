431c609ce932019758d16b2f963a74f9e9d27525
jetsalinas <joseenrico.salinas@gmail.com>
2017-08-20 17:56:27 +0800

    Defined Ballot schema

--- main.py
+++ main.py
@@ -1,7 +1,51 @@
+"""
+    Copyright
+    Jose Salinas & Jasper Refuerzo
+    08/19/201
+"""
+
 from flask import Flask
 from flask import render_template
 
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+
+#############
+#  CONFIGS  #
+#############
+
 app = Flask(__name__)
+
+database = SQLAlchemy(app)
+marshmallow = Marshmallow(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///local.db'
+
+#############
+#  SCHEMAS  #
+#############
+
+class Ballot(database.Model):
+    __tablename__ = "ballots"
+
+    ballotID = database.Column(database.Integer, primary_key=True)
+    ballotBatch = database.Column(database.Integer)
+    ballotLName = database.Column(database.String(50))
+    ballotFName = database.Column(database.String(50))
+    ballotTime = database.Column(database.datetime)
+
+    ballotPresident = database.Column(database.Integer)
+    ballotVicePresident = database.Column(database.Integer)
+    ballotSecretary = database.Column(database.Integer)
+    ballotTreasurer = database.Column(database.Integer)
+    ballotAuditor = database.Column(database.Integer)
+
+class BallotSchema(marshmallow.ModelSchema):
+        class Meta:
+            model = Ballot
+
+#########
+#  APP  #
+#########
 
 @app.route('/')
 def main_page():
