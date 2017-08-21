395e5876eafe5e756122abcba4415da69b269549
jetsalinas <joseenrico.salinas@gmail.com>
2017-08-21 20:00:45 +0800

    Added login functionality

--- main.py
+++ main.py
@@ -6,14 +6,14 @@
 
 import os
 import csv
+from keys import SESSION_SECRET_KEY
 from datetime import datetime
 
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template, jsonify
+from flask import session, request, redirect, url_for
 
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
-from flask import jsonify
 
 #############
 #  CONFIGS  #
@@ -24,6 +24,8 @@ app = Flask(__name__)
 database = SQLAlchemy(app)
 marshmallow = Marshmallow(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///local.db'
+app.secret_key = SESSION_SECRET_KEY
+
 
 #############
 #  SCHEMAS  #
@@ -100,14 +102,14 @@ with open("candidatelist.csv") as candidate_csv:
         candidateTotalVotes = 0,
         candidateTime = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
         ))
-
+'''
 database.create_all()
 for ballot in ballots:
     database.session.add(ballot)
 for candidate in candidates:
     database.session.add(candidate)
 database.session.commit()
-
+'''
 #LOAD SCHEMAS
 ballot_schema = BallotSchema()
 candidate_schema = CandidateSchema()
@@ -116,24 +118,44 @@ candidate_schema = CandidateSchema()
 #  APP  #
 #########
 
+def validate_login(username, password):
+    return True
+
 @app.route('/')
 def main_page():
-    return render_template('login.html')
+    return redirect(url_for('login_page'))
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login_page():
+    #SKIP LOG IN PAGE IF A USER IS ALREADY LOGGED IN
+    if session['username']:
+        return redirect(url_for('vote_page'))
+    #PROCESS LOGIN REQUESTS
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        if validate_login(username=username, password=password):
+            session['username'] = request.form['username']
+            session['logged_in'] = True
+            return redirect(url_for('vote_page'))
+    #SHOW LOGIN PAGE
     return render_template('login.html')
 
 @app.route('/vote')
 def vote_page():
-    return render_template('vote.html')
+    if session['username']:
+        return render_template('vote.html')
+    return redirect(url_for('login_page'))
 
 @app.route('/verify')
 def verify_page():
-    return render_template('verify.html')
+    if session['username']:
+        return render_template('verify.html')
+    return redirect(url_for('login_page'))
 
 @app.route('/logout')
 def logout_page():
+    session['username'] = None
     return render_template('logout.html')
 
 @app.route('/debug')
